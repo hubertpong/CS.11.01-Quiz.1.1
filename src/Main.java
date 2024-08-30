b41883085fb002779d044e7a367b8eabb7768396
hubert <hubertp2027@student.cis.edu.hk>
2024-08-30 12:13:03 +0800

    Hubert's Formative

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,31 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Hubert
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+int age;
+String firstName;
+String favouriteFood;
+
+System.out.println("Please enter your age:");
+age = scanner.nextInt();
+
+System.out.println("Enter your name:");
+firstName = scanner.next();
+
+System.out.println("What is your favourite food:");
+favouriteFood = scanner.next();
+
+System.out.println("First name:" +firstName);
+System.out.println("Age:" +age);
+System.out.println("Favourite food:" + favouriteFood);
 
 
 
